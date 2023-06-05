b34c8ad72498b7fe9ef6184e02051e029912b087
geheim01 <nils.wenzlitschke@googlemail.com>
2023-06-05 10:36:28 +0200

    WIP

--- synthesizers/cgan/model.py
+++ synthesizers/cgan/model.py
@@ -2,163 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from sklearn.neighbors import KNeighborsClassifier
-
 from synthesizers.utils.training import (
     buildDataCTST,
     generate_and_plot_data,
     make_subplots,
     synthetic_dataset,
 )
+from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy_lib
+from tensorflow_privacy.privacy.optimizers.dp_optimizer import (
+    DPGradientDescentGaussianOptimizer,
+)
 
-## Define Conditional GAN with Keras Tensorflow
-
-# ## Define Hyperparameters here
-# ## Maybe you have to check if it is working to change number of features, because we used two in our paper
-# num_features = trainX.shape[2]
-
-# # I used the same shape for latent_dim as for seq_length
-# # Maybe you have to change something in the code if you want to change that behavior
-# seq_length = 60
-# latent_dim = seq_length
-
-# batch_size = 32
-# # LSTM hidden units
-# hidden_units = 16
-
-# # fill in your data here
-# # trainX = 0
-# # label_trainX = 0
-
-# # Mos are the positive samples and nonMos are the negative samples
-# # If you want to use the GANmonitor you have to stick with this naming
-# # Else you have to change them in the code
-# # trainmos = 0
-# # trainnomos = 0
-# # testmos = 0
-# # testnomos = 0
-
-# # Dataset to for the GAN
-# dataset = tf.data.Dataset.from_tensor_slices((trainX, label_trainX))
-# dataset = dataset.shuffle(buffer_size=1024).batch(batch_size)
-# dataset
-
-
-# LSTM Generator
-
-
-# class Generator(tf.keras.Model):
-#     def __init__(self, hidden_units, seq_length, latent_dim, num_features, classes=2):
-#         self.hidden_units = hidden_units
-#         self.seq_length = seq_length
-#         self.latent_dim = latent_dim
-#         self.num_features = num_features
-#         self.classes = classes
-
-#     def build(self):
-#         # connect latent_space with a neuronal Net
-#         in_label = tf.keras.layers.Input(shape=(1,))
-#         # embedding for categorical input
-#         layer = tf.keras.layers.Embedding(self.classes, 2)(in_label)
-
-#         layer = tf.keras.layers.Dense(self.seq_length)(layer)
-
-#         # reshape to additional channel
-#         layer = tf.keras.layers.Reshape((self.seq_length, 1))(layer)
-
-#         # orginal sequenz input
-#         in_seq = tf.keras.layers.Input(shape=(self.latent_dim,))
-
-#         # connect latent_space with a neuronal Net
-#         seq = tf.keras.layers.Dense(self.seq_length * self.num_features)(in_seq)
-
-#         # activation
-#         seq = tf.keras.layers.LeakyReLU()(seq)
-
-#         # reshape to additional channel
-#         seq = tf.keras.layers.Reshape((self.seq_length, self.num_features))(seq)
-
-#         # merge input with label
-#         merge = tf.keras.layers.Concatenate()([seq, layer])
-
-#         # LSTM layer block
-#         rnn = tf.keras.layers.LSTM(self.hidden_units, return_sequences=True)(merge)
-
-#         rnn = tf.keras.layers.LSTM(self.hidden_units, return_sequences=True)(rnn)
-
-#         # output lineare activation layer
-#         out_layer = tf.keras.layers.Dense(self.num_features, dtype="float64")(rnn)
-
-#         model = tf.keras.models.Model([in_seq, in_label], out_layer)
-
-#         return model
-
-
-# # LSTM Discrimnator
-
-
-# class Discriminator(tf.keras.Model):
-#     def __init__(
-#         self,
-#         seq_length,
-#         num_features,
-#         classes=2,
-#         opt=tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5),
-#     ):
-#         self.seq_length = seq_length
-#         self.num_features = num_features
-#         self.classes = classes
-#         self.opt = opt
-
-#     def build(self):
-#         # connect label input with a neuronal Net
-#         in_label = tf.keras.layers.Input(shape=(1,))
-#         # embedding for categorical input
-#         layer = tf.keras.layers.Embedding(self.classes, 2)(in_label)
-
-#         layer = tf.keras.layers.Dense(self.seq_length)(layer)
-
-#         layer = tf.keras.layers.Reshape((self.seq_length, 1))(layer)
-
-#         # input sequenz
-#         in_seq = tf.keras.layers.Input(shape=(self.seq_length, self.num_features))
-
-#         # merge label and sequenz
-#         merge = tf.keras.layers.Concatenate()([in_seq, layer])
-
-#         # time Series Classification from Scratch https://arxiv.org/abs/1611.06455
-#         # convolutional layer block
-#         # You can play here with different filters and kernel sizes
-#         conv1 = tf.keras.layers.Conv1D(
-#             filters=32, kernel_size=8, strides=1, padding="same"
-#         )(merge)
-#         conv1 = tf.keras.layers.BatchNormalization()(conv1)
-#         conv1 = tf.keras.layers.ReLU()(conv1)
-
-#         # convolutional layer block
-#         conv2 = tf.keras.layers.Conv1D(
-#             filters=64, kernel_size=5, strides=1, padding="same"
-#         )(conv1)
-#         conv2 = tf.keras.layers.BatchNormalization()(conv2)
-#         conv2 = tf.keras.layers.ReLU()(conv2)
-
-#         # convolutional layer block
-#         conv3 = tf.keras.layers.Conv1D(
-#             filters=32, kernel_size=3, strides=1, padding="same"
-#         )(conv2)
-#         conv3 = tf.keras.layers.BatchNormalization()(conv3)
-#         conv3 = tf.keras.layers.ReLU()(conv3)
-
-#         # average pooling
-#         gap = tf.keras.layers.GlobalAveragePooling1D()(conv3)
-
-#         # output activation
-#         out_layer = tf.keras.layers.Dense(1)(
-#             gap
-#         )  # omit sigmoid activation here to use Numerical stable Binary Crossentropy loss function
-
-#         model = tf.keras.models.Model([in_seq, in_label], out_layer)
-
-#         return model
+GradientDescentOptimizer = tf.compat.v1.train.GradientDescentOptimizer
 
 
 class ConditionalGAN(tf.keras.Model):
@@ -207,12 +62,19 @@ class ConditionalGAN(tf.keras.Model):
         )
 
         # Train the discriminator.
-        with tf.GradientTape() as tape:
+        with tf.GradientTape(persistent=True) as tape:
             predictions = self.discriminator(
                 [fake_seq_and_real_seq, seq_one_hot_label_comb]
             )
+            # var_list = self.discriminator.trainable_variables
             d_loss = self.loss_fn(labels, predictions)
+
+            # grads = self.d_optimizer.compute_gradients(
+            #     d_loss, var_list, gradient_tape=disc_tape
+            # )
+
         grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+
         self.d_optimizer.apply_gradients(
             zip(grads, self.discriminator.trainable_weights)
         )
@@ -389,6 +251,10 @@ class GANMonitor(tf.keras.callbacks.Callback):
         randomTestMos,
         randomTestNoMos,
         num_features,
+        # noise_multiplier,
+        batch_size,
+        save_path,
+        dp=False,
         num_seq=1,
         seq_length=18,
     ):
@@ -401,10 +267,14 @@ class GANMonitor(tf.keras.callbacks.Callback):
         self.randomTestMos = randomTestMos
         self.randomTestNoMos = randomTestNoMos
         self.seq_length = seq_length
+        self.save_path = save_path
         self.scorelist = []
         # how many sequences you want to plot in the grid
         self.num_seq = num_seq
         self.num_features = num_features
+        # self.noise_multiplier = noise_multiplier
+        self.batch_size = batch_size
+        # self.dp = dp
 
     def on_epoch_end(self, epoch, logs=None):
         label_mos = tf.ones((self.randomTrainNoMos.shape[0], 1))
@@ -426,6 +296,18 @@ class GANMonitor(tf.keras.callbacks.Callback):
         stressData = self.model.generator([self.randomTrainMos, label_mos])
         nostressData = self.model.generator([self.randomTrainMos, label_nomos])
 
+        # # Compute the privacy budget expended.
+        # if self.dp:
+        #     if self.noise_multiplier > 0.0:
+        #         eps, _ = compute_dp_sgd_privacy_lib.compute_dp_sgd_privacy(
+        #             60000, self.batch_size, self.noise_multiplier, epoch, 1e-5
+        #         )
+        #         print("For delta=1e-5, the current epsilon is: %.2f" % eps)
+        #     else:
+        #         print("Trained with DP-SGD but with zero noise.")
+        # else:
+        #     print("Trained with vanilla non-private SGD optimizer")
+
         # if you want to perform classifier two sample test as well
         nn_train, nn_label, nn_test, nn_label_test = buildDataCTST(
             synthTrainMos,
@@ -446,10 +328,9 @@ class GANMonitor(tf.keras.callbacks.Callback):
         c2stScore = neigh.score(nn_test, nn_label_test)
         self.scorelist.append(c2stScore)
 
-        if c2stScore <= min(self.scorelist):
-            self.model.generator.save(
-                f"models/sixty_seconds/cond_generator_{epoch}_{c2stScore}"
-            )
+        if c2stScore <= min(self.scorelist) and c2stScore < 0.8:
+            print(c2stScore)
+            self.model.generator.save(f"{self.save_path}generator_{epoch}_{c2stScore}")
 
         if (epoch) % 250 == 0:
             plt.plot(self.scorelist)
@@ -457,4 +338,4 @@ class GANMonitor(tf.keras.callbacks.Callback):
             generate_and_plot_data(
                 stressData, nostressData, self.num_seq, self.seq_length
             )
-            self.model.generator.save(f"models/sixty_seconds/cond_generator_{epoch}")
+            self.model.generator.save(f"{self.save_path}generator_{epoch}_{c2stScore}")
